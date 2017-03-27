4eb0d5475f1cbd17f41adc87219120ef5742bd9a
guoxiaowei <guo.xiaowei@jyall.com>
2017-03-27 16:45:08 +0800

    update

---
+++ VZone/VZone/config.h
@@ -0,0 +1,18 @@
+//
+//  config.h
+//  VZone
+//
+//  Created by guo xiaowei on 2017/3/27.
+//  Copyright © 2017年 guo xiaowei. All rights reserved.
+//
+
+#ifndef config_h
+#define config_h
+
+
+
+#define kScreenW [UIScreen mainScreen].bounds.size.width
+#define kScreenH [UIScreen mainScreen].bounds.size.height
+
+
+#endif /* config_h */
